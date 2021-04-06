e745be8502bcbc677cdebac5cf502f55a1740fe4
Johan <johan@macair2020>
2021-04-06 11:24:04 +0200

    reconcile meta model in test

--- src/query-builder/meta.test.ts
+++ src/query-builder/meta.test.ts
@@ -18,35 +18,19 @@ const meta: M.MetaQueryUnit[] = [
     filters: [],
   },
   {
+    alias: "t1",
     entity: "UserStatus",
     table: "user_status",
-    alias: "t1",
-    join: { entity: "User", field: "status", optional: false },
+
     fields: [{ name: "name", column: "col_name" }],
-    filters: [{ name: "name", column: "name", value: "ok" }],
+    filters: [{ name: "name", column: "col_name", value: "ok" }],
+    join: { entity: "User", field: "status", optional: false },
   },
 ];
 
 test("to meta", () => {
   // todo filters
-  expect(M.toMeta("User", q.User, model)).toEqual([
-    {
-      entity: "User",
-      table: "user",
-      alias: "t0",
-      fields: [{ column: "first_name", name: "firstName" }],
-      filters: [],
-      join: undefined,
-    },
-    {
-      alias: "t1",
-      entity: "UserStatus",
-      fields: [{ column: "name", name: "name" }],
-      filters: [{ name: "name", column: "name", value: "ok" }],
-      join: { entity: "User", field: "status", optional: false },
-      table: "user_status",
-    },
-  ]);
+  expect(M.toMeta("User", q.User, model)).toEqual(meta);
 });
 
 test("to query", () => {
@@ -54,6 +38,6 @@ test("to query", () => {
     "SELECT t0.first_name, t1.col_name",
     "FROM user as t0",
     "JOIN user_status as t1 ON t1.id=t0.status",
-    'WHERE 1 AND t1.name="ok"',
+    'WHERE 1 AND t1.col_name="ok"',
   ]);
 });
