e5e7fc7a4231666b3af6b5641dc01de14a1276f3
Johan <johan@macair2020>
2021-06-18 15:04:16 +0200

    moe main

---
+++ src/lib/main.ts
@@ -0,0 +1,31 @@
+// this file is used for the package. The server should use the other files directly
+import * as Connection from "./database/connection";
+import { Database } from "./database/type";
+import * as Exec from "./exec";
+import * as T from "./type";
+import * as MigrationService from "@nexys/sql-migrations";
+
+export class Main {
+  s: Connection.SQL;
+  model: T.Entity[];
+
+  constructor(c: Database, model: T.Entity[]) {
+    this.s = new Connection.SQL(
+      c.host,
+      c.username,
+      c.password,
+      c.database,
+      c.port
+    );
+    this.model = model;
+  }
+
+  mutate = (m: T.Mutate) => Exec.mutate(m, this.model, this.s);
+
+  query = (q: T.Query) => Exec.exec(q, this.model, this.s);
+
+  applyMigration = (migrations: MigrationService.Type.Migration[]) =>
+    MigrationService.Migrations.runMigrations(migrations, this.s);
+}
+
+export default Main;
