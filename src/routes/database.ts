24b7d6ff8e254e793ebdb2410aeee4caab268010
Johan <johan@macair2020>
2021-07-14 09:29:13 +0200

    close #37

--- src/routes/database.ts
+++ src/routes/database.ts
@@ -5,29 +5,36 @@ import bodyParser from "koa-body";
 import * as Middleware from "../middleware";
 import * as DatabaseService from "../service/database";
 import * as U from "../service/database/utils";
+import * as V from "@nexys/validation";
 
 const router: Router = new Router();
 
 router.all("/get", Middleware.isAuth, async (ctx) => {
   ctx.body = DatabaseService.get(ctx.state.jwtContent);
 });
-router.all("/set", Middleware.isAuth, bodyParser(), async (ctx) => {
-  const { body } = ctx.request;
+router.all(
+  "/set",
+  Middleware.isAuth,
+  bodyParser(),
+  V.Main.isShapeMiddleware({
+    database: {},
+    username: {},
+    password: {},
+    host: {},
+  }),
+  async (ctx) => {
+    const { body } = ctx.request;
 
-  if (!body) {
-    ctx.status = 400;
-    ctx.body = { error: "payload expected" };
+    const e = U.checkDatabase(body);
+
+    if (e.length > 0) {
+      ctx.status = 400;
+      ctx.body = e;
+      return;
+    }
+
+    ctx.body = await DatabaseService.set(ctx.state.jwtContent, body);
   }
-
-  const e = U.checkDatabase(body);
-
-  if (e.length > 0) {
-    ctx.status = 400;
-    ctx.body = e;
-    return;
-  }
-
-  ctx.body = await DatabaseService.set(ctx.state.jwtContent, body);
-});
+);
 
 export default router.routes();
