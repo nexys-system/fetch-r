9646924563ebc1adf79a99217ca2afe36a8b1459
Johan <johan@macair2020>
2021-07-20 14:00:55 +0200

    query builder in import

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,10 @@
 // this file is used for the package. The server should use the other files directly
 export * as Connection from "./database/connection";
 export * as Exec from "./exec";
-export * as Typrd from "./type";
+export * as Type from "./type";
+export * as QueryBuilder from "./query-builder";
 export * as MigrationService from "@nexys/sql-migrations";
+
 import Main from "./main";
 
 export default Main;
---
+++ src/lib/query-builder/index.ts
@@ -0,0 +1,7 @@
+export * as Aggregate from "./aggregate";
+export * as Meta from "./meta";
+export * as Parse from "./parse";
+export * as ParseMutate from "./parse-mutate";
+export * as Mutate from "./mutate";
+export * as Type from "./type";
+export * as Utils from "./utils";
